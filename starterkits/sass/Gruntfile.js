a71e1835e10274231a90c26f287e2897d5557a84
Ivan Lewaschiw <ivan@chuva-inc.com>
2015-09-16 19:58:36 -0300

    Issues #2102895: Configure the base Gruntfile.js to execute future tasks.

---
+++ starterkits/sass/Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function (grunt) {
+
+  grunt.registerTask('prepare-sass-starterkit', 'Copies all bootstrap-sass related files to related starterkits folders', function () {
+    
+  });
+
+  //grunt.registerTask('install', ['prepare-sass-starterkit']);
+};
